114a55082c79437cf748ba0606de88d77e8cf680
Archana8757 <60030634+Archana8757@users.noreply.github.com>
2025-06-18 01:48:23 +0530

    added about section

---
+++ about.js
@@ -0,0 +1,20 @@
+// let icon = document.querySelectorAll(".social-icons i");
+// icon.forEach(e =>
+//     e.addEventListener('mouseover', (pro) => {
+//         pro.target.style.color = "var(--text-color)";
+//         pro.target.style.backgroundColor = "var(--main-color)";
+//         pro.target.style.boxShadow = "0 0 25px var(--main-color)";
+//         pro.target.style.transform = "scale(1.2)translateY(-5px)";
+//         pro.target.style.padding = "padding: 6rem 2rem !important";
+//     })
+// );
+
+// icon.forEach(e =>
+//     e.addEventListener('mouseout', (pro) => {
+//         pro.target.style.color = "var(--main-color)";
+//         pro.target.style.backgroundColor = "transparent";
+//         pro.target.style.boxShadow = "";
+//         pro.target.style.transform = "";
+//         pro.target.style.padding = "";
+//     })
+// );
